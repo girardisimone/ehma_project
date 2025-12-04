366fe83dca6ada0e99a09ad93f659fd48dec0e63
Stella <mariastella.ferrari@mail.polimi.it>
2025-12-04 11:09:14 +0100

    bottone e suoni gemme e portali

--- unityProject/Assets/Scripts/Gem.cs
+++ unityProject/Assets/Scripts/Gem.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class Gem : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public AudioClip pickupSound; // Trascina il file audio qui nell'Inspector della Gemma
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            // 1. Suona l'audio in un oggetto temporaneo
+            if (pickupSound != null)
+            {
+                // PlayClipAtPoint crea un oggetto audio temporaneo alla posizione della gemma
+                AudioSource.PlayClipAtPoint(pickupSound, transform.position);
+            }
+
+            // 2. Logica Punteggio (La tua logica esistente o richiamo al Manager)
+            // Nota: Se usi questo, rimuovi la logica di distruzione dal PlayerController per non farlo due volte!
+            ScoreManager.GemCount++;
+            if (ScoreManager.Instance != null)
+                ScoreManager.Instance.UpdateScoreText(ScoreManager.GemCount);
+
+            // 3. Distruggi la gemma
+            Destroy(gameObject);
+        }
     }
 }
